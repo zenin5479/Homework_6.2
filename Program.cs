872d6cef00d7c6e3eb585f8737082851e435586c
zenin5479 <zenin5479@gmail.com>
2025-09-11 09:34:03 +0300

    Update 0.6 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 // Обработка текста
 // Дан текст. Текст разбит на слова, предполагается, что слово – это группа символов,
@@ -6,16 +7,22 @@
 // Текст может содержать и несколько предложений, признаком конца предложения является точка
 // Составить программу для обработки текстовой информации по поставленному условию
 // Каждое слово "один" заменить на "два"
-// Слово с наибольшим количеством гласных.
-
+// Слово с наибольшим количеством гласных
 
 namespace Homework_6._2
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+   internal class Program
+   {
+      static void Main(string[] args)
+      {
+         string fileEnter = "parable.txt";
+         string pathEnter = Path.GetFullPath(fileEnter);
+         string fileInput = "finish.txt";
+         string pathInput = Path.GetFullPath(fileInput);
+
+
+
+         Console.ReadKey();
+      }
+   }
 }
