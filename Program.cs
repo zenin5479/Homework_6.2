80124057a9fef1b755fc8af307dc28af6fb8576e
zenin5479 <zenin5479@gmail.com>
2025-09-14 11:45:07 +0300

    Update 2.5 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 
 // Обработка текста
 // Дан текст. Текст разбит на слова, предполагается, что слово – это группа символов,
@@ -34,10 +35,94 @@ namespace Homework_6._2
 
       public static string[] ReplaceWord(string path, string oldWord, string newWord)
       {
-         
-         
-         
-         return new string[] { };
+
+         string stroka = null;
+         string[] arrayDouble = { };
+         FileStream filestream = File.Open(path, FileMode.Open, FileAccess.Read);
+         if (filestream == null || filestream.Length == 0)
+         {
+            Console.WriteLine("Ошибка при открытии файла для чтения");
+         }
+         else
+         {
+            StreamReader streamReader = new StreamReader(filestream);
+            while (streamReader.Peek() >= 0)
+            {
+               stroka = streamReader.ReadLine();
+               //Console.WriteLine(stroka);
+            }
+
+            // Определение количества столбцов в строке разделением строки на подстроки по пробелу
+            // Символ пробела
+            char symbolSpace = ' ';
+            // Счетчик символов
+            int symbolСount = 0;
+            // Количество столбцов в строке
+            int сolumn = 0;
+            if (stroka != null)
+            {
+               //Console.WriteLine("Исходный строковый массив {0}:", nameArray);
+               //Console.WriteLine(stroka);
+               while (symbolСount < stroka.Length)
+               {
+                  if (symbolSpace == stroka[symbolСount])
+                  {
+                     сolumn++;
+                  }
+
+                  if (symbolСount == stroka.Length - 1)
+                  {
+                     сolumn++;
+                  }
+
+                  symbolСount++;
+               }
+
+               //Console.WriteLine("Количество столбцов {0}:", сolumn);
+
+               // Разделение строки на подстроки по пробелу и конвертация подстрок в double
+               //Console.WriteLine("Массив вещественных чисел {0}:", nameArray);
+               // Одномерный массив вещественных чисел
+               arrayDouble = new double[сolumn];
+               // Построитель строк
+               StringBuilder stringModified = new StringBuilder();
+               // Счетчик символов обнуляем
+               symbolСount = 0;
+               // Количество столбцов в строке обнуляем
+               сolumn = 0;
+               while (symbolСount < stroka.Length)
+               {
+                  if (symbolSpace != stroka[symbolСount])
+                  {
+                     stringModified.Append(stroka[symbolСount]);
+                  }
+                  else
+                  {
+                     string subLine = stringModified.ToString();
+                     arrayDouble[сolumn] = Convert.ToDouble(subLine);
+                     //Console.Write(arrayDouble[сolumn] + " ");
+                     stringModified.Clear();
+                     сolumn++;
+                  }
+
+                  if (symbolСount == stroka.Length - 1)
+                  {
+                     string subLine = stringModified.ToString();
+                     arrayDouble[сolumn] = Convert.ToDouble(subLine);
+                     //Console.Write(arrayDouble[сolumn]);
+                     stringModified.Clear();
+                     сolumn++;
+                  }
+
+                  symbolСount++;
+               }
+            }
+
+            streamReader.Close();
+            //Console.WriteLine();
+         }
+
+         return arrayDouble;
       }
    }
 }
