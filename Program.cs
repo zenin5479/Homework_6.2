0242ec31557cf2875a224d3986518701c63defec
zenin5479 <zenin5479@gmail.com>
2025-09-19 13:31:47 +0300

    Update 15.6 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -47,7 +47,7 @@ namespace Homework_6._2
             string word = words[i];
             int currentVowelCount = 0;
             int j = 0;
-            while (j < word.ToLower().Length)
+            while (j < word.Length)
             {
                char c = word.ToLower()[j];
                if (vowels.IndexOf(c) != -1)
