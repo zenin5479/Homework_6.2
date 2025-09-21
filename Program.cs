698c1d023f48854d89c2b39d8eb7d4bc7cff9549
zenin5479 <zenin5479@gmail.com>
2025-09-21 15:34:05 +0300

    Update 20.8 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -71,7 +71,7 @@ namespace Homework_6._2
                         //   countVowel++;
                         //}
 
-                        
+                        // Вариант 2
                         // Сравниваем значения string используя метод Compare игнорируя регистр
                         if (string.Compare(vowels[l].ToString(), partString, StringComparison.OrdinalIgnoreCase) == 0)
                         {
@@ -85,6 +85,7 @@ namespace Homework_6._2
                         //   countVowel++;
                         //}
 
+                        // Вариант 2
                         // Сравниваем значения string используя метод Equals(string) игнорируя регистр
                         //if (string.Equals(vowels[l].ToString(), partString, StringComparison.OrdinalIgnoreCase))
                         //{
