e360bcb930e9069c3a2fa916d0aa4244895501b8
zenin5479 <zenin5479@gmail.com>
2025-09-21 22:19:21 +0300

    Update 22.7 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -29,7 +29,6 @@ namespace Homework_6._2
          string[,] replaceArray = ReplaceWord(enterArray, oldWord, newWord);
          InputArrayString(replaceArray);
          Console.WriteLine();
-
          string wordsArray = FindWordWithMostVowels(replaceArray);
          Console.WriteLine("Слово с наибольшим количеством гласных: {0}", wordsArray);
 
@@ -300,122 +299,19 @@ namespace Homework_6._2
          return arrayString;
       }
 
-      public static string[,] ReplaceWord(string[,] path, string oldWord, string newWord)
+      public static string[,] ReplaceWord(string[,] arrayString, string oldWord, string newWord)
       {
          // Одномерный массив строк
-         string[,] arrayString = { };
+         //string[,] arrayString = { };
          // Чтение файла за одну операцию
-         string[] allLines = File.ReadAllLines(path);
+         string[] allLines = File.ReadAllLines(arrayString);
          if (allLines == null)
          {
             Console.WriteLine("Ошибка при открытии файла для чтения");
          }
          else
          {
-            Console.WriteLine("Исходный массив строк");
-            int indexLines = 0;
-            while (indexLines < allLines.Length)
-            {
-               allLines[indexLines] = allLines[indexLines];
-               Console.WriteLine(allLines[indexLines]);
-               indexLines++;
-            }
 
-            // Разделение строки на подстроки по пробелу для определения количества столбцов в строке
-            int[] sizeArray = new int[allLines.Length];
-            char symbolSpace = ' ';
-            int countRow = 0;
-            int countSymbol = 0;
-            int countСolumn = 0;
-            while (countRow < allLines.Length)
-            {
-               string line = allLines[countRow];
-               while (countSymbol < line.Length)
-               {
-                  if (symbolSpace == line[countSymbol])
-                  {
-                     countСolumn++;
-                  }
-
-                  if (countSymbol == line.Length - 1)
-                  {
-                     countСolumn++;
-                  }
-
-                  countSymbol++;
-               }
-
-               sizeArray[countRow] = countСolumn;
-               Console.WriteLine("В строке {0} количество столбцов {1}", countRow, countСolumn);
-               countСolumn = 0;
-               countRow++;
-               countSymbol = 0;
-            }
-
-            // Поиск максимального элемента массива
-            // Cчитаем, что максимум - это первый элемент массива
-            int max = sizeArray[0];
-            int columns = 0;
-            while (columns < sizeArray.Length)
-            {
-               if (max < sizeArray[columns])
-               {
-                  max = sizeArray[columns];
-               }
-
-               columns++;
-            }
-
-            Console.WriteLine("Максимум равен: {0}", max);
-
-            // Разделение строк на подстроки по пробелу и заполнение двумерного массива строк элементами
-            // Измерение массива (0) - количество строк, измерение массива (1) - количество столбцов, равное максимуму
-            Console.WriteLine("Двумерный массив строк");
-            StringBuilder stringModified = new StringBuilder();
-            arrayString = new string[allLines.Length, max];
-            char spaceCharacter = ' ';
-            int row = 0;
-            int column = 0;
-            int countCharacter = 0;
-            while (row < arrayString.GetLength(0))
-            {
-               string line = allLines[row];
-               while (column < sizeArray[row])
-               {
-                  while (countCharacter < line.Length)
-                  {
-                     if (spaceCharacter == line[countCharacter])
-                     {
-                        string subLine = stringModified.ToString();
-                        arrayString[row, column] = subLine;
-                        Console.Write(arrayString[row, column] + " ");
-                        stringModified.Clear();
-                        column++;
-                     }
-                     else
-                     {
-                        stringModified.Append(line[countCharacter]);
-                     }
-
-                     if (countCharacter == line.Length - 1)
-                     {
-                        string subLine = stringModified.ToString();
-                        arrayString[row, column] = subLine;
-                        Console.Write(arrayString[row, column]);
-                        stringModified.Clear();
-                        column++;
-                     }
-
-                     countCharacter++;
-                  }
-
-                  countCharacter = 0;
-               }
-
-               Console.WriteLine();
-               column = 0;
-               row++;
-            }
 
             // Поиск и замена слов в массиве строк
             int k = 0;
