258de09ba745e00f7eb7af543802ca61ff42a5a0
zenin5479 <zenin5479@gmail.com>
2025-09-21 22:44:40 +0300

    Update 23.5 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.IO;
 using System.Text;
-using System.Threading.Channels;
 
 // Обработка текста
 // Дан текст. Текст разбит на слова, предполагается, что слово – это группа символов,
